6df5e3c913bca5dce34755a3c8d3a14a2a087a91
vbth7777 <vbth7777@gmail.com>
2024-02-03 18:30:32 +0700

    03/02/2024

--- scripts/custom-script.lua
+++ scripts/custom-script.lua
@@ -1,8 +1,16 @@
 local utils = require 'mp.utils'
 
 function openURLInBrowser(url)
+    local chromeCommand = "google-chrome-stable"
+    local result, err = utils.subprocess({args = {"pgrep", "-x", "google-chrome-beta"}})
+    if result.error or result.status ~= 0 then
+        mp.msg.error("Failed to check if google-chrome-beta is running: " .. (err or ""))
+    else
+        if result.stdout ~= "" then
+            chromeCommand = "google-chrome-beta"
+        end
+    end
     local profileDirectory = "Profile 1"
-    local chromeCommand = "google-chrome-beta"
     local args = {chromeCommand, "--profile-directory=" .. profileDirectory, url}
     local result = utils.subprocess({args = args})
 
