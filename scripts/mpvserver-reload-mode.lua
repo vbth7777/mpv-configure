5e0cdebc4fa0d7672f17d5d105678d77f469b307
vbth7777 <vbth7777@gmail.com>
2024-06-20 00:08:23 +0700

    20/06/2024

---
+++ scripts/mpvserver-reload-mode.lua
@@ -0,0 +1,15 @@
+local mp = require("mp")
+local utils = require("mp.utils")
+
+function reload()
+	local url = "http://localhost:9789/reload"
+	mp.osd_message("Reloaded")
+	local result, err = utils.subprocess({ args = { "curl", "-X", "POST", url } })
+	if result.error or result.status ~= 0 then
+		mp.msg.error("Failed to reload: " .. (err or ""))
+		mp.osd_message("Failed to reload")
+		return
+	end
+end
+
+mp.add_key_binding("ctrl+q", "Reload", reload)
